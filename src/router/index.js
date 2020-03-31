3f08b5ec44ebe5fe2adce54f1f9f288badea5ced
xieyuquan1996 <xieyuquan1996@users.noreply.github.com>
2020-03-31 13:08:54 +0800

    删除没用的一些router

--- src/router/index.js
+++ src/router/index.js
@@ -65,15 +65,6 @@ const routes = [{
     name: 'address',
     component: () =>
         import('../views/address/Address.vue')
-},
-{
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-        import( /* webpackChunkName: "about" */ '../views/About.vue')
 }
 ]
 
