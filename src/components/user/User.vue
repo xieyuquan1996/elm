6de05a79b14a1900ad7bb750aede56168bf7d7d9
xieyuquan1996 <xieyuquan1996@users.noreply.github.com>
2020-04-03 17:15:26 +0800

    增加用户页

---
+++ src/components/user/User.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="uesr">
+    <div class="user-content" @click="loginOrOut">
+      <div>
+        <img :src="avatar" class="img">
+      </div>
+      <div>
+        {{userName}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      userName: '123456',
+      avatar: '',
+      loginStatus: true
+    }
+  },
+  methods: {
+    loginOrOut(){
+      if(this.loginStatus){
+        this.loginOut()
+      } else {
+        this.login()
+      }
+    },
+    login(){
+      console.log('登录')
+    },
+    loginOut(){
+      console.log('退出登录成功')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import './user.styl'
+</style>
